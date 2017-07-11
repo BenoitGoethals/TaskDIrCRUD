78b14a038db9dff86231138a92407d334144ecc0
benoi <benoit.goethals@gmail.com>
2017-07-11 21:22:37 +0200

    fgf

--- TaskDIrCRUD/Program.cs
+++ TaskDIrCRUD/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,5 +16,49 @@ namespace TaskDIrCRUD
             // Make XML TREE-
             // proces parallel files
         }
-    }
+        /*
+        private static void createAlphabetFolders(File parent, int start, int end, int deepth)
+        {
+
+            if (deepth <= 0)
+            {
+                return;
+            }
+
+            for (int i = start; i < end; i++)
+            {
+
+                // create the folder
+                String folderName = "" + ((char)i);
+                File folder = new File(parent, folderName);
+                System.out.println("creating: " + folder.getPath());
+                folder.mkdirs();
+
+                // call recursively
+                createAlphabetFolders(folder, start, end, deepth - 1);
+            }
+        }
+        */
+
+/*
+ ublic static void main(String[] args) {
+File root = new File("C:\\SO");
+List<String> alphabet = new ArrayList<String>();
+for (int i = 0; i < 26; i++) {
+alphabet.add(String.valueOf((char)('a' + i)));
+}
+
+final int depth = 3;
+mkDirs(root, alphabet, depth);
+}
+
+public static void mkDirs(File root, List<String> dirs, int depth) {
+if (depth == 0) return;
+for (String s : dirs) {
+File subdir = new File(root, s);
+subdir.mkdir();
+mkDirs(subdir, dirs, depth - 1);
+}
+}*/
+}
 }
